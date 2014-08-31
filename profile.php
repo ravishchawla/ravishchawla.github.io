ad12c68a62f351fc3d00241a1849e6b06e67b642
Ravish Chawla <rchawla8@github.com>
2014-08-30 23:44:33 -0400

    Initial Commit

---
+++ profile.php
@@ -0,0 +1,96 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ravish Chawla Profile</title>
+	<link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="./bootstrap/style.css" rel="stylesheet">
+	<link href="./style.css" rel="stylesheet" type="text/css">
+
+	<style>
+		h3 small {
+			color: #000;
+		}
+		
+		h3.gray {
+			color: #777;
+		}
+		
+		h4 {
+			text-indent: 25px each-line;
+			color: #777;
+		}
+		
+		p {
+			color: #777777;
+			text-indent: 25px;
+		}
+		
+		p a {
+			font-style: oblique;
+		}
+	</style>
+	
+</head>
+<body>	
+	<!-- Site header and navigation -->
+<div id="wrap">	
+	<div class="header">
+	<?php 
+	include("header.html");
+	?>
+	</div>
+
+	<div class="middle body">
+		<div class="container">
+			<div class="col-md-7 col-lg-9">
+				<!--<p class="lead">I am a senior at the Georgia Institute of Technology, completing Undergraduate and, followed by the pursuit of Masters in Computer Science.
+								I have had experience in multiple languages, including C, C#, Java, PHP, JavaScript, and SQL. 
+								Furthermore, I have also implemented projects in these languages, through game and mobile apps development. 
+								Specifically, I have developed games in XNA coupled with C#, and C on small-bit processors, as well as mobile apps written in Android and published on the Play Store. 
+								Moreover, I have had experience in Databases, with knowledge in SQL and research in ETL Applications. 
+								I have also explored Big Data processing through work on the Hadoop Architecture with Map Reduce algorithms to process large datasets. 
+								Aside from my programming and development experience, I have also gained knowledge in Computing Principles, most significant of which include Algorithms and Data Structures, Relational and NoSQL Database Systems, Memory and Computer management, and Communications Networking.
+								My experience in Computer Science comprises of many projects, languages, and concepts, each of which I will continue to expand over the course of my time at Georgia Tech, and my careers during that time. 
+				</p>
+				-->
+		
+
+			<h3>Education</h3>
+					<h3 class="gray">Georgia Institute of Technology — Atlanta, GA (May 2012 - Dec 2015)</h3>
+						<h4><i>Completing Bachelor of Science in Computer Science</i></h4>
+							<h4><small>Threads:Information Technology and Devices</small></h4>
+				<hr></hr>
+				
+				<h3>Work</h3>
+					<h3 class="gray">Zynga — San Francisco, CA (May 2014 - Aug 2014)</h3>
+						<h4><i>Software Engineering Intern</i></h4>
+							<h4><small>Completed development of backend tools for load balancers and server management across clusters
+							<br><br>Implemented an automation service within a game that delivers items to a targeted set of daily users by leveraging mobile push notifications</small></h4>
+				<hr></hr>
+				
+		
+			</div>
+			<div class="col-md-5 col-lg-3">
+				<img src="profile_picture.jpg" class="visible-md-block visible-lg-block" width="200" height="277"</img>
+			</div>
+		</div>
+	</div>
+	
+	<div class="footer">
+	<?php
+	include("footer.html");
+	?>
+	</div>
+	
+</div>
+	
+</body>
+</html>                                  		
+
+<script src="./jquery/jquery-2.1.1.min.js"></script>
+<script src="./bootstrap/js/bootstrap.min.js"></script>
+<script src="./script.js"></script>
+
+</body>
+</html>
---
+++ projects.php
@@ -0,0 +1,103 @@
+			
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ravish Chawla Profile</title>
+	<link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="./bootstrap/style.css" rel="stylesheet">
+	<link href="./style.css" rel="stylesheet" type="text/css">
+	
+	<style>
+				
+		h3 small {
+			text-indent: 25px;
+		}
+		
+		p {
+			color: #777777;
+			text-indent: 25px;
+		}
+		
+		p a {
+			font-style: oblique;
+		}
+		
+	</style>
+</head>
+<body>	
+	<!-- Site header and navigation -->
+<div id="wrap">	
+	<div class="header">
+	<?php 
+	include("header.html");
+	?>
+	</div>
+
+	<div class="middle body">
+		<div class="container">
+			<h3>Android Apps</h3>
+				<h3><small>Developed and published different apps on the Google Play store, that used different backend technologies and services, 
+					such as the Parse backend framework for cloud data storage, and Ad monetization through Google's AdMob platform</small></h3>	
+				<p>App Link: <a href=https://play.google.com/store/apps/details?id=com.stacksmashers.greenbook>Greenbook - Money management app</a></p>
+			<hr></hr>
+			
+			<h3>Page Rank</h3>
+				<h3><small>Wrote Map reduce algorithms for Big data processing for Page Rank in Hadoop, to process the Wikipedia page-links dataset and Twitter-Memetracket dataset. 
+						   The page rank algorithm ranked websites based on how many other websites link to it, by treating hyperlinks as edges and websites as nodes. 
+						   The data collected from the Map Reduce implementation was then evaluated against an Apache Giraph implementation to compare memory and speed processing of the algorithms. 
+				</small></h3>
+				<p>Presentaion Link: <a href=http://1drv.ms/1qqHfTd> Result and analysis of Page rank and comparison of implementations.</a></p>
+			
+			<hr></hr>
+			
+			<h3>BitTorrent Application</h3>
+				<h3><small>Developed a BitTorrent application in C#, that can download and upload torrent files from secure trackers. The application 
+						   used a Bencoding library to read torrent file data, and connected to peers using a persistent TCP connection. File download
+						   took place with parallel unchoked connections to multiple peers, and handled for packet corruption, and uploaded file chunks 
+						   as they were received. 
+				</small></h3>
+			
+			<hr></hr>
+			<h3>Games in C and C#/XNA</h3>
+				<h3><small>Developed games with C for small 8-bit processors, employing Direct memory access for display. 
+						   <br></br>Also developed several games in C# with XNA, that worked with an XBOX-360 Game controller for player movement. 
+				</small></h3>
+			
+			<hr></hr>
+			
+			<h3>ETL Workflow Optimization</h3>
+				<h3><small>Researched Optimization in the ETL workflow process, used for processing and loading data into Data Warehouses. 
+						   Examined different areas and methods of optimization, which included heuristic and exhaustive methods, and compared overall improved performance
+						   between them and other proposed approaches. Also looked at what current research and being conducted for this topic, and how it is being used 
+						   in the industry. 
+				</small></h3>
+				<p>Research Paper Link: <a href=http://1drv.ms/1tZe6Aa> Examination of Research in ETL Workflow Optimization</a></p>
+			<hr></hr>
+			
+			<h3>ArangoDB</h3>
+				<h3><small>Researched a new NoSQL Technology called ArangoDB, which provides Graph data storage in terms of nodes and edges, 
+						   as well as documents. Compared it to other databases, such as CouchDB and MongoDB, analysing what features they provide, 
+						   and how performance compares between each for large amounts of data.
+				</small></h3>
+				<p>Research Paper Link: <a href=http://1drv.ms/1nbYmEX> ArangoDB - A new NoSQL Database System</a></p>
+			
+		</div>
+	</div>
+	
+	<div class="footer">
+	<?php
+	include("footer.html");
+	?>
+	</div>
+	
+</div>
+	
+</body>
+</html>                                  		
+
+<script src="./jquery/jquery-2.1.1.min.js"></script>
+<script src="./bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
